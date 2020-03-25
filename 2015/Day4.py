968693c35b0dbb2c34fa0e44ca5be9e29e2145a7
Illia Rahavoi <irahavoi@gmail.com>
2020-03-25 16:15:56 -0400

    Day4

---
+++ 2015/Day4.py
@@ -0,0 +1,14 @@
+import hashlib
+input = 'yzbqklnj'
+
+hash = hashlib.md5(input.encode())
+print(hash.hexdigest())
+
+for i in range(282749, 100000000):
+    test = input + str(i)
+    hash = hashlib.md5(test.encode()).hexdigest()
+    
+    if(hash.startswith('000000')):
+        print(hashlib.md5(test.encode()).hexdigest())
+        print(i)
+        break
